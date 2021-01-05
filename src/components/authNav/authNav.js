f2ba98e773dec113303ba1800b4792488488e1df
AkoniHakeem <akozymayor@gmail.com>
2021-01-05 08:52:56 +0100

    commit  refresh

---
+++ src/components/authNav/authNav.js
@@ -0,0 +1,12 @@
+import "./authNav.css"
+
+const AuthNav = (props) => {
+    return(
+        <>
+            <button className="auth-nav-button">Sign Up</button>
+            <button className="auth-nav-button">Log In</button>
+        </>
+    )
+}
+
+export default AuthNav
---
+++ src/components/context/appContext.js
@@ -0,0 +1,6 @@
+import React, { useReducer, useState } from 'react'
+import CartReducer from '../Reducers/cartReducer';
+
+
+export const AppContext = React.createContext();
+
