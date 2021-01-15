79dbe16c6ae27f3da48d4ef2fc16738fc4158fd4
AkoniHakeem <akozymayor@gmail.com>
2021-01-15 10:25:07 +0100

    added config

---
+++ src/config.js
@@ -0,0 +1,23 @@
+/* 
+ * Manges the environments variable
+*/
+
+//  Dependencies
+const config = {}
+
+config.environments = {
+    "development" : {
+        backendUrl: process.env.backendUrl || "http://localhost:3001"
+    },
+    "staging" : {
+        backendUrl: process.env.backendUrl || "http://localhost:3001"
+    },
+    "production" : { // We should set the https port always here
+        backendUrl: process.env.backendUrl
+    }
+}
+
+const envName = process.env.NODE_ENV? process.env.NODE_ENV.trim().toLocaleLowerCase() : "staging";
+const environment = typeof(config.environments[envName]) == "object"? config.environments[envName] : config.environments.staging ;
+
+module.exports = environment   
